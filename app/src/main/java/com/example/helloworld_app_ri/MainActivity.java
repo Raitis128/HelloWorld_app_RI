9889c752cb093fc3329cc13fd0eedd08dc65514f
Raitis128 <raitisiziks@gmail.com>
2022-09-10 13:19:37 +0300

    Add show my name buttton

--- app/src/main/java/com/example/helloworld_app_ri/MainActivity.java
+++ app/src/main/java/com/example/helloworld_app_ri/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.helloworld_app_ri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.textView = findViewById(R.id.textView);
+    }
+
+    public void onBtnClick(View view) {
+        this.textView.setText("My name is Raitis!");
     }
 }
